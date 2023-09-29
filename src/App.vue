80e18a29174f39d7c9c6d4db1e71e8154c475ebe
Akmal Akbarzoda <akbrzda@icloud.com>
2023-09-29 14:27:38 +0500

    Update CataVue

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,47 @@
 <script setup>
 import {ref} from 'vue';
-const catText = ref('')
+import Tags from './components/Tags.vue';
+const catText = ref('');
+const types = ref(['random', 'gif',  'says', 'tags']);
+const selectedType = ref('random'); // Default selected type
 
 
 function getImgWithText() {
   const img = document.querySelector(".catImg");
-  img.src = "https://cataas.com/cat/says/" + catText.value;
+  const test = document.querySelector(".test");
+  const inputText = document.querySelector("#input");
+  const constBuffer = Math.random();
+
+  if (selectedType.value === 'says') {
+    img.src = "https://cataas.com/cat/says/" + catText.value + "?cache=" + constBuffer;
+  } else if(selectedType.value === 'random') {
+    img.src = "https://cataas.com/cat" + catText.value + "?cache=" + constBuffer;
+  } else if (selectedType.value === 'gif') {
+    img.src = "https://cataas.com/cat/gif" + "?cache=" + constBuffer;
+  } else if (selectedType.value === 'tags') {
+    img.src = "https://cataas.com/cat/" + catText.value +  "?cache=" + constBuffer;
+  } else {
+    test.innerHTML = "Не сработало";
+  }
 }
 
-function getImgWithTag() {
-  const img = document.querySelector(".catImg");
-  img.src = "https://cataas.com/cat/" + catText.value;
-}
 </script>
 
 <template>
   <div class="cat-block">
-<input type="text" v-model="catText" id="#input"/>
-<button @click="getImgWithText()">Press me if you want cats with your text</button>
-<!-- <button @click="getImgWithTag()">Press me if you want cats with tag</button> -->
- <img  class="catImg"/>
- </div>
-</template>
+    <div class="types-block">
+    <label v-for="(type) in types" :for="type" > {{ type.toUpperCase() }}
+      <input  class="photo-type" type="radio" :value="type" name="type" :id="type" v-model="selectedType">
+    </label>
+  </div>
+  <a href="tags.html" v-if="selectedType === 'tags'">Tags *Press Me*</a>
+  <!-- <Tags v-if="selectedType === 'tags'"/> -->
 
-<style scoped>
-.cat-block {
-  display: flex;
-  flex-direction: column;
-  gap: 40px;
-  align-items: center;
-}
-input {
-  background-color: #fff;
-  width: 400px;
-  height: 40px;
-  color: #000;
-  border-color: #eee;
-}
-</style>
+    <div class="input-block">
+      <input type="text" v-model="catText" id="#input" v-if="selectedType === 'says' || selectedType === 'tags'"/>
+      <button @click="getImgWithText()" >Generate Photo</button>
+    </div>
+    
+  </div>
+  <img  class="catImg"/>
+</template>
---
+++ src/components/Tags.vue
@@ -0,0 +1,32 @@
+<script setup>
+import { ref } from 'vue'
+
+async function tags() {
+  const requestURL =
+    "https://cataas.com/api/tags";
+  const request = new Request(requestURL);
+
+  const response = await fetch(request);
+  const tagsListJson = await response.json();
+
+  tagsList(tagsListJson);
+}
+function tagsList(obj) {
+  const mainSection = document.querySelector(".card");
+  const tagsJsonList = obj;
+  const myList = document.createElement("ul");
+  for (const tag of tagsJsonList) {
+    const listItem = document.createElement("li");
+    listItem.textContent = tag;
+    myList.appendChild(listItem);
+  }
+  mainSection.appendChild(myList);
+}
+
+tags();
+</script>
+
+<template>
+  <div class="card">
+  </div>
+</template>
